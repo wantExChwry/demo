631a73b4433d79e79e63a0e893ddaef42f4c3eea
wurongy <1711690995@qq.com>
2023-11-20 21:05:32 +0800

    feat: 新功能

--- demo/src/main/java/com/example/demo/git/gitTest.java
+++ demo/src/main/java/com/example/demo/git/gitTest.java
@@ -20,11 +20,6 @@ public class gitTest {
         return "login";
     }
 
-    @RequestMapping("get")
-    public String get(){
-        return "login";
-    }
-
     @RequestMapping("ge2t")
     public String getWx2(){
         return "getWx2";
