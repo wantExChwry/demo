18a1fd079ef12871f2c4209c1788756b48d15bc0
wurongy <1711690995@qq.com>
2023-12-16 13:31:28 +0800

    feat: cherry-pick层数3
    (cherry picked from commit 9e05a5c55af93cc6c5262c3f44701fcde6b2636b)

--- demo/src/main/java/com/example/demo/git/gitCheck.java
+++ demo/src/main/java/com/example/demo/git/gitCheck.java
@@ -31,7 +31,6 @@ public class gitCheck {
     public String updateTset2(){
         System.out.println("cherry-pick");
         System.out.println("cherry-pick");
-        System.out.println("cherry-pick");
         return "cherry-pick";
     }
 }
