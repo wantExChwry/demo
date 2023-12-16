40d9a47a5227ca7f9e1da109f6af95c7a860aea6
wurongy <1711690995@qq.com>
2023-12-16 13:21:32 +0800

    feat: cherry-pic添加层数2

--- demo/src/main/java/com/example/demo/git/gitCheck.java
+++ demo/src/main/java/com/example/demo/git/gitCheck.java
@@ -29,6 +29,11 @@ public class gitCheck {
     }
     @RequestMapping("/updateTst2")
     public String updateTset2(){
+        System.out.println("cherry-pick");
+        System.out.println("cherry-pick");
+        System.out.println("cherry-pick");
+        System.out.println("cherry-pick");
+        System.out.println("cherry-pick");
         return "cherry-pick";
     }
 }
