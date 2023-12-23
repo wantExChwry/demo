7c65254321c32d3312613f29952664223b14af4a
wurongy <1711690995@qq.com>
2023-12-23 20:56:12 +0800

    feat: dev xin

--- demo/src/main/java/com/example/demo/git/gitCheck.java
+++ demo/src/main/java/com/example/demo/git/gitCheck.java
@@ -33,6 +33,8 @@ public class gitCheck {
         System.out.println("cherry-pick");
         System.out.println("cherry-pick");
         System.out.println("cherry-pick");
+        System.out.println("a");
+        System.out.println("b");
         return "cherry-pick";
     }
 
